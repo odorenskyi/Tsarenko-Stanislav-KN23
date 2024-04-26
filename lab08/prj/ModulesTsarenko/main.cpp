463f9b34b61831016309ea0ba493bb53dbfaa546
StasTsarenko <stastsarenko05@gmail.com>
2024-04-26 12:45:10 +0300

    add Report, TestSuit and SoftWare

--- lab08/prj/ModulesTsarenko/main.cpp
+++ lab08/prj/ModulesTsarenko/main.cpp
@@ -36,34 +36,34 @@ void beaufort(double* speed, int length) {
         if (max < speed[i])
             max = speed[i];
     }
-    cout << "Max is: " << max << endl;
+    cout << "Максимум: " << max << endl;
 
     if (max < 0.3)
-        cout << "Score of Beaufort is 0!" << endl;
+        cout << "Бал Бофорта 0!" << endl;
     else if (0.3 < max && max < 1.5)
-        cout << "Score of Beaufort is 1!" << endl;
+        cout << "Бал Бофорта 1!" << endl;
     else if (1.6 < max && max < 3.4)
-        cout << "Score of Beaufort is 2!" << endl;
+        cout << "Бал Бофорта 2!" << endl;
     else if (3.4 < max && max < 5.4)
-        cout << "Score of Beaufort is 3!" << endl;
+        cout << "Бал Бофорта 3!" << endl;
     else if (5.5 < max && max < 7.9)
-        cout << "Score of Beaufort is 4!" << endl;
+        cout << "Бал Бофорта 4!" << endl;
     else if (8 < max && max < 10.7)
-        cout << "Score of Beaufort is 5!" << endl;
+        cout << "Бал Бофорта 5!" << endl;
     else if (10.8 < max && max < 13.8)
-        cout << "Score of Beaufort is 6!" << endl;
+        cout << "Бал Бофорта 6!" << endl;
     else if (13.9 < max && max < 17.1)
-        cout << "Score of Beaufort is 7!" << endl;
+        cout << "Бал Бофорта 7!" << endl;
     else if (17.2 < max && max < 20.7)
-        cout << "Score of Beaufort is 8!" << endl;
+        cout << "Бал Бофорта 8!" << endl;
     else if (20.8 < max && max < 24.4)
-        cout << "Score of Beaufort is 9!" << endl;
+        cout << "Бал Бофорта 9!" << endl;
     else if (24.5 < max && max < 28.4)
-        cout << "Score of Beaufort is 10!" << endl;
+        cout << "Бал Бофорта 10!" << endl;
     else if (28.5 < max && max < 32.6)
-        cout << "Score of Beaufort is 11!" << endl;
+        cout << "Бал Бофорта 11!" << endl;
     else if (max >= 32.7)
-        cout << "Score of Beaufort is 12!" << endl;
+        cout << "Бал Бофорта 12!" << endl;
 }
 
 unsigned int bits(unsigned int num) {
